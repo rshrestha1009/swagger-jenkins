a71f7a75ab49ead267170c6e5bf4fe6ebbb6523d
rshrestha156 <rajesh.shrestha1008@gmail.com>
2023-10-30 17:00:02 -0400

    first commit

---
+++ src/main/java/com/swaggertutorial/swaggerapi/AddressBookResource.java
@@ -0,0 +1,31 @@
+package com.swaggertutorial.swaggerapi;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RestController
+@RequestMapping("/api")
+public class AddressBookResource {
+
+    ConcurrentMap<String, Contact> contacts = new ConcurrentHashMap<>();
+
+    @GetMapping("{id}")
+    public Contact getContact(@PathVariable String id) {
+        return contacts.get(id);
+    }
+
+    @GetMapping("/")
+    public List<Contact> getAllContacts() {
+        return new ArrayList<>(contacts.values());
+    }
+
+    @PostMapping("/")
+    public Contact addContact(@RequestBody Contact contact) {
+        contacts.put(contact.getId(), contact);
+        return contact;
+    }
+}
